715a6aba8ef2c3f558986a8266747abaa6bdb5dc
Milan Kubík <email@kubikmilan.sk>
2020-07-26 17:11:25 +0200

    Add first weird subcommand implementation

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,59 @@ struct Opts {
 #[derive(Clap)]
 enum SubCommand {
     Set(Set),
+    Get(Get),
+    Clear(Clear),
 }
 
 #[derive(Clap)]
 struct Set {
     key: String,
     value: String,
+}
+
+#[derive(Clap)]
+struct Get {
+    key: String,
+}
+
+#[derive(Clap)]
+struct Clear {
+    key: String,
+}
+
+trait StorageCommand {
+    fn execute(&self) -> Result<bool, &'static str>;
+}
+
+impl StorageCommand for Set {
+    fn execute(&self) -> Result<bool, &'static str> {
+        println!("Setting a value [{}] to key [{}]", self.value, self.key);
+        Ok(true)
+    }
+}
+
+impl StorageCommand for Get {
+    fn execute(&self) -> Result<bool, &'static str> {
+        println!("Getting the value for key [{}]", self.key);
+        Ok(true)
+    }
+}
+
+impl StorageCommand for Clear {
+    fn execute(&self) -> Result<bool, &'static str> {
+        println!("Clearing the key [{}]", self.key);
+        Ok(true)
+    }
+}
+
+impl StorageCommand for SubCommand {
+    fn execute(&self) -> Result<bool, &'static str> {
+        match self {
+            SubCommand::Set(cmd) => cmd.execute(),
+            SubCommand::Get(cmd) => cmd.execute(),
+            SubCommand::Clear(cmd) => cmd.execute(),
+        }
+    }
 }
 
 fn main() {
@@ -26,9 +73,5 @@ fn main() {
         _ => println!("Value of verbose: {}", opts.verbose),
     }
 
-    match opts.subcmd {
-        SubCommand::Set(s) => {
-            println!("Key => value: {} => {}", s.key, s.value);
-        }
-    }
+    let _result = opts.subcmd.execute();
 }
