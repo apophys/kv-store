b4c79ea3683e966c990d7ef9491272841ae3284b
Milan Kubík <email@kubikmilan.sk>
2020-07-26 15:21:33 +0200

    Initial clap structure to parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::Clap;
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Set(Set),
+}
+
+#[derive(Clap)]
+struct Set {
+    key: String,
+    value: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.verbose {
+        _ => println!("Value of verbose: {}", opts.verbose),
+    }
+
+    match opts.subcmd {
+        SubCommand::Set(s) => {
+            println!("Key => value: {} => {}", s.key, s.value);
+        }
+    }
 }
