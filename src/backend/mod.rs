27e08c95706037f28e57bc0f6c757dae31771613
Milan Kubík <email@kubikmilan.sk>
2020-09-19 21:25:46 +0200

    Introduce backend specific errors and rework return types
    Adds customized result types to the method of the adapter
    implementation.
    
    Reworked the function bodies to pass the errors up the stack.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -2,15 +2,35 @@ use crate::config::Config;
 
 pub mod redis;
 
-pub trait BackendAdapter {
-    fn new() -> Self
-    where
-        Self: Sized;
-    fn set(&mut self, key: &str, value: &str) -> String;
-    fn get(&mut self, key: &str) -> String;
-    fn clear(&mut self, key: &str) -> String;
+#[derive(Debug)]
+pub enum BackendAdapterError {
+    NetworkUnavailable(std::io::Error),
+    ParseError(std::str::Utf8Error),
 }
 
-pub fn get_backend_adapter(_options: &Config) -> Box<dyn BackendAdapter> {
-    Box::new(redis::RedisConnection::new())
+pub type BackendAdapterResult<T> = Result<T, BackendAdapterError>;
+
+pub trait BackendAdapter {
+    fn new() -> BackendAdapterResult<Self>
+    where
+        Self: Sized;
+    fn set(&mut self, key: &str, value: &str) -> BackendAdapterResult<String>;
+    fn get(&mut self, key: &str) -> BackendAdapterResult<String>;
+    fn clear(&mut self, key: &str) -> BackendAdapterResult<String>;
+}
+
+pub fn get_backend_adapter(_options: &Config) -> BackendAdapterResult<Box<dyn BackendAdapter>> {
+    Ok(Box::new(redis::RedisConnection::new()?))
+}
+
+impl From<std::str::Utf8Error> for BackendAdapterError {
+    fn from(error: std::str::Utf8Error) -> Self {
+        BackendAdapterError::ParseError(error)
+    }
+}
+
+impl From<std::io::Error> for BackendAdapterError {
+    fn from(error: std::io::Error) -> Self {
+        BackendAdapterError::NetworkUnavailable(error)
+    }
 }
--- src/backend/redis.rs
+++ src/backend/redis.rs
@@ -1,4 +1,4 @@
-use super::BackendAdapter;
+use super::*;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::str;
@@ -10,51 +10,39 @@ pub struct RedisConnection {
 }
 
 impl BackendAdapter for RedisConnection {
-    fn new() -> Self {
-        RedisConnection {
-            conn: TcpStream::connect("127.0.0.1:6379")
-                .expect("Couldn't connect to redis on 127.0.0.1:6379"),
-        }
+    fn new() -> BackendAdapterResult<Self> {
+        Ok(RedisConnection {
+            conn: TcpStream::connect("127.0.0.1:6379")?,
+        })
     }
 
-    fn set(&mut self, key: &str, value: &str) -> String {
+    fn set(&mut self, key: &str, value: &str) -> BackendAdapterResult<String> {
         println!("Setting value [{}] for key [{}] in redis", &key, &value);
         self.conn
-            .write(format!("SET {} {}\r\n", &key, &value).as_bytes())
-            .expect("Fail to communicate with redis");
+            .write(format!("SET {} {}\r\n", &key, &value).as_bytes())?;
         let mut result = [0; 512];
-        self.conn
-            .read(&mut result)
-            .expect("Fail to communicate with redis");
-        let result_str = str::from_utf8(&result).expect("Handle me better");
+        self.conn.read(&mut result)?;
+        let result_str = str::from_utf8(&result)?;
 
-        result_str.to_string()
+        Ok(result_str.to_string())
     }
 
-    fn get(&mut self, key: &str) -> String {
-        self.conn
-            .write(format!("GET {}\r\n", &key).as_bytes())
-            .expect("Fail to communicate with redis");
+    fn get(&mut self, key: &str) -> BackendAdapterResult<String> {
+        self.conn.write(format!("GET {}\r\n", &key).as_bytes())?;
         let mut result = [0; 512];
-        self.conn
-            .read(&mut result)
-            .expect("Fail to communicate with redis");
-        let result_str = str::from_utf8(&result).expect("Handle me better");
+        self.conn.read(&mut result)?;
+        let result_str = str::from_utf8(&result)?;
 
-        result_str.to_string()
+        Ok(result_str.to_string())
     }
 
-    fn clear(&mut self, key: &str) -> String {
-        self.conn
-            .write(format!("DEL {}\r\n", &key).as_bytes())
-            .expect("Fail to communicate with redis");
+    fn clear(&mut self, key: &str) -> BackendAdapterResult<String> {
+        self.conn.write(format!("DEL {}\r\n", &key).as_bytes())?;
         let mut result = [0; 512];
-        self.conn
-            .read(&mut result)
-            .expect("Fail to communicate with redis");
+        self.conn.read(&mut result)?;
 
-        let result_str = str::from_utf8(&result).expect("Handle me better");
+        let result_str = str::from_utf8(&result)?;
 
-        result_str.to_string()
+        Ok(result_str.to_string())
     }
 }
