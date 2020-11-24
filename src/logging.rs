3b69b36e4400777c9a4c3383242a317dae244f02
Milan Kubík <email@kubikmilan.sk>
2020-11-24 01:50:40 +0100

    Reuse the unit return from simplelog init

--- src/logging.rs
+++ src/logging.rs
@@ -5,11 +5,9 @@ use crate::config;
 pub type ConfigResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 pub fn configure_logger(config: &config::Config) -> ConfigResult<()> {
-    simplelog::TermLogger::init(
+    Ok(simplelog::TermLogger::init(
         config.logging_level,
         Default::default(),
         simplelog::TerminalMode::Stderr,
-    )?;
-
-    Ok(())
+    )?)
 }
